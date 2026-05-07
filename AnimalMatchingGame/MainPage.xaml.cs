fe742e37a5d9697fa87c6161179b7bd37b7e81d7
dannygray161 <dieseldan14@gmail.com>
2026-05-07 15:57:46 -0500

    feat: add game comparison logic and win alert

--- AnimalMatchingGame/MainPage.xaml.cs
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace AnimalMatchingGame
+﻿using Microsoft.Maui.Layouts;
+
+namespace AnimalMatchingGame
 {
     public partial class MainPage : ContentPage
     {
@@ -24,8 +26,40 @@
           
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
+        private async Task Button_Clicked(object sender, EventArgs e)
         {
+            if(sender is Button buttonClicked) // if an animal button is clicked
+            {
+                if(!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch = false)) // if the button is not blank, and we havent matched yet
+                {
+                    buttonClicked.BackgroundColor = Colors.Red; 
+                    lastClicked = buttonClicked; // track what we just clicked
+                    findingMatch = true; // we're looking for a match now!
+                }
+                else
+                {
+                    if((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text)) // new button clicked, and it matched!
+                    {
+                        matchesFound++; // increment matches for score
+                        lastClicked.Text = " "; // reset the logic
+                        buttonClicked.Text = " "; // reset the logic
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
+                }
+                if(matchesFound == 8) // all matches found, reset the game to start and alert user
+                {
+                    matchesFound = 0;
+                    AnimalButtons.IsVisible = false;
+                    PlayAgainButton.IsVisible = true;
+
+                    await DisplayAlertAsync("Alert", "You Win!", "OK");
+                }
+            }
 
         }
     }
